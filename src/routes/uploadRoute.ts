cdf3d74135a7e47d618657e5c331d2e62d537b4d
Cleitson Lima <cleitson.ftw@gmail.com>
2024-08-30 23:43:33 -0300

    nao precisa

--- src/routes/uploadRoute.ts
+++ src/routes/uploadRoute.ts
@@ -4,9 +4,7 @@ import uploadController from "../controller/uploadController";
 
 const uploadRouter = Router();
 
-uploadRouter.get("/", (req: Request, res: Response) => {
-  res.send({ message: "upload" });
-});
+
 uploadRouter.post("/", uploadMiddleware.authUploadMiddleware, uploadController.uploadImage);
 
 export default uploadRouter;
